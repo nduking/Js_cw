142544346fe853c14406bd3e972d34253c343ee6
nduking <114291880+nduking@users.noreply.github.com>
2025-05-01 18:17:53 +0100

    assignment pactice

---
+++ app.js
@@ -0,0 +1,24 @@
+//const fullName= "Nduka Asoh Obewu";
+const bioData = {
+    fullName: `Nduka Asoh Obewu`,
+    Sex: "Male",
+    Married: true,
+    Age: 36,
+    Blood: "O+",
+    Genotype: "As",
+    Occupation: "Technology Support",
+  };
+  console.log(bioData);
+
+  // Datatypes of the Objects
+
+  console.log(
+    typeof bioData.fullName,
+    typeof bioData.Sex,
+    typeof bioData.Married,
+    typeof bioData.Age,
+    typeof bioData.Blood,
+    typeof bioData.Genotype,
+    typeof bioData.Occupation
+  );
+  
