0cd52e31ccdc68aa5261f476096cdc5801583e51
nduking <114291880+nduking@users.noreply.github.com>
2025-05-03 11:44:42 +0100

    conditonal statement

---
+++ conditional.js
@@ -0,0 +1,15 @@
+const age = 10;
+if (age >= 18) {
+  console.log(`user can enter the application because their age =${age}`);
+} else {
+  console.log(`user cannot enter the app because their age =${age}`);
+}
+
+const weather = `rainy`;
+if (weather === `rainy`) {
+  console.log(`The weather is currently ${weather} please use a raincoat`);
+} else if (weather === `sunny`) {
+  console.log(`The weather is currently ${weather} please use an umbrella`);
+} else {
+  console.log(`The weather is currently ${weather} please enjoy your day`);
+}
